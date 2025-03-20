f36af61e25683baf1e397c218c2a3fbac3ca111a
ankitsharma38 <ankitsharma7805@gmail.com>
2025-03-21 01:15:43 +0530

    more

---
+++ src/components/SlidingText/SlidingText.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const SlidingText = () => {
+  return (
+    <Box
+      sx={{
+        overflow: "hidden",
+        position: "relative",
+     //    backgroundColor: "#f0f0f0",
+        py: 2,
+        mb: -6,
+      }}
+    >
+      {/* Sliding Text Container */}
+      <Box
+        sx={{
+          display: "flex",
+          whiteSpace: "nowrap",
+          animation: "slide 20s linear infinite",
+          "@keyframes slide": {
+            "0%": {
+              transform: "translateX(100%)",
+            },
+            "100%": {
+              transform: "translateX(-100%)",
+            },
+          },
+        }}
+      >
+        {/* Text to Slide */}
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 600,
+            color: "#333",
+            pr: 30, // Add spacing between repeated text
+          }}
+        >
+          Chakrapani Panchakarma Yoga Nisargopchar Mahavidyalaya
+        </Typography>
+        {/* Repeat the text for a seamless effect */}
+
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 600,
+            color: "#333",
+            pr: 10, // Add spacing between repeated text
+          }}
+        >
+          Admission Open  
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default SlidingText;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,13 @@ import Course from "../../components/Course/Course";
 import Testimonial from "../../components/Testimonials/Testimonial";
 import Eligibility from "../../components/Criteria/Eligibility";
 import Achievements from "../../components/Achievements/Achievements";
+import SlidingText from "../../components/SlidingText/SlidingText";
 
 export default function Home() {
   return (
     <div className="relative overflow-hidden">
       <Hero />
+      <SlidingText/>
       <Course/>
       <Achievements/>
       <BlogList />
