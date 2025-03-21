3682049a5c2834347b33e0f50765e47f1cff950e
ankitsharma38 <ankitsharma7805@gmail.com>
2025-03-21 18:38:14 +0530

    space

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -67,7 +67,7 @@ const Course = () => {
   }
 
   return (
-    <section id="courses" className="py-16 bg-#fafffc scroll-mt-16">
+    <section id="courses" className="py-22 bg-#fafffc scroll-mt-16">
       <div className="container mx-auto px-4">
         <motion.h2
           initial={{ opacity: 0, y: 20 }}
